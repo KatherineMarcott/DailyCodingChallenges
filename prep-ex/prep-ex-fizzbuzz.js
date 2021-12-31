1e4b0aa3004ee374a427d453d51ccb1407b0f340
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2021-12-31 12:12:06 -0500

    add prep example using fizzbuzz question

---
+++ prep-ex/prep-ex-fizzbuzz.js
@@ -0,0 +1,17 @@
+// PREP 
+
+// A number 1 to number, if 3 fizz, if 5 buzz, 3&5 fizzbuzz
+
+// Parameter: num, whole number, positive num
+// Returns: console.log each num
+// Examples: function fizzBuzz (num) {
+
+// }
+
+//fizzBuzz(5) - 1,2,fizz,4,buzz
+//fizzBuzz(15) - 1,2,fizz,4,buzz,fizz,7,8,9,buzz,11,fizz,13,14,fizzbuzz
+
+// Pseudo Code: function fizzBuzz(num){
+    //loop
+    // conditionals %3&5, %3, %5
+//}
