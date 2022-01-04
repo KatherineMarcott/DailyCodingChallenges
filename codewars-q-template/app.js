4fa5d26f53f2e110526712e26e06025c0a7e8867
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-01-04 14:57:46 -0500

    add html/js template for formatting codewars

---
+++ codewars-q-template/app.js
@@ -0,0 +1,26 @@
+// Instructions // 
+/* */
+ 
+// Summary //
+/* */
+
+// P: 
+
+// R: 
+
+// E: 
+// 
+// 
+
+/* P:  */
+
+// Solution for Codewars//
+
+
+  
+// Solution for showing on console in Browser // 
+
+
+
+/* Tests */ 
+
