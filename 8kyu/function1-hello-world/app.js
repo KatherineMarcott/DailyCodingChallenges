5dc92a8a60dafe0e0f4386947b4e9ac2a2a2c0ab
Katherine Marcott <katherinesoftwareengineer@gmail.com>
2022-05-30 11:23:43 -0400

    add organizational template, instructions, solutions, and tests to function 1 hello world 8kyu

---
+++ 8kyu/function1-hello-world/app.js
@@ -0,0 +1,29 @@
+// Instructions // 
+
+/* Description:
+Make a simple function called greet that returns the most-famous "hello world!". */
+ 
+// Summary //
+/* Make a simple function called greet that returns the most-famous "hello world!".*/
+
+// P: hello world must be undercase to pass testing. 
+
+// R: return 'hello world!' 
+
+// E: 
+// greet() => "hello world!"
+
+/* P: Create a function
+      return 'hello world!' */
+
+// Solution for Codewars//
+
+const greet1 = () => 'hello world!'
+  
+// Solution for showing on console in Browser // 
+
+const greet = () => console.log('hello world!')
+
+/* Tests */ 
+
+greet() // => "hello world!"
